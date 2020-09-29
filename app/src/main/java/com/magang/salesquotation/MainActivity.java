81a3ff1aa4ab3c74c048fefd138f0d28fb68faaf
tiofani99 <tiofani03@gmail.com>
2020-09-29 20:48:35 +0700

    nambah intent ke add sales

--- app/src/main/java/com/magang/salesquotation/MainActivity.java
+++ app/src/main/java/com/magang/salesquotation/MainActivity.java
@@ -1,14 +1,35 @@
 package com.magang.salesquotation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.magang.salesquotation.ui.addsales.AddSalesQuotationActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_add_data)
+    Button btnAddData;
+    @BindView(R.id.swipe_refresh_home)
+    SwipeRefreshLayout swipeRefreshLayout;
+    @BindView(R.id.rv_home)
+    RecyclerView rvHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        btnAddData.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, AddSalesQuotationActivity.class));
+        });
     }
 }
---
+++ app/src/main/java/com/magang/salesquotation/ui/addsales/AddSalesQuotationActivity.java
@@ -0,0 +1,16 @@
+package com.magang.salesquotation.ui.addsales;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.magang.salesquotation.R;
+
+public class AddSalesQuotationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_sales_quotation);
+    }
+}
