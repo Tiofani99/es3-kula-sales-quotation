6d7c35136ed27abf44421ebefc5de92de6077872
tiofani99 <tiofani03@gmail.com>
2020-09-29 21:33:12 +0700

    buat desain tambah data

--- app/src/main/java/com/magang/salesquotation/ui/addsales/AddSalesQuotationActivity.java
+++ app/src/main/java/com/magang/salesquotation/ui/addsales/AddSalesQuotationActivity.java
@@ -2,6 +2,7 @@ package com.magang.salesquotation.ui.addsales;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.magang.salesquotation.R;
@@ -12,5 +13,47 @@ public class AddSalesQuotationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_sales_quotation);
+        initTitle();
+    }
+
+    private void initTitle() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle(getResources().getString(R.string.add_data));
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void showAlertDialog() {
+        String dialogTitle;
+        String dialogMessage;
+
+        dialogTitle = "Kembali";
+        dialogMessage = "Apakah anda yakin ingin kembali ?";
+
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        alertDialogBuilder.setTitle(dialogTitle);
+        alertDialogBuilder
+                .setMessage(dialogMessage)
+                .setCancelable(false)
+                .setPositiveButton(getString(R.string.yes), (dialogInterface, i) -> finish())
+                .setNegativeButton(getString(R.string.no), (dialogInterface, i) -> dialogInterface.cancel());
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        showAlertDialog();
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        showAlertDialog();
     }
 }
