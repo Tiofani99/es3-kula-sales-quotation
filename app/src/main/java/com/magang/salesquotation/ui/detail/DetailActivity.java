1609a743ceea68153e608f06937b2ae435459902
tiofani99 <tiofani03@gmail.com>
2020-09-30 12:51:58 +0700

    update detail

--- app/src/main/java/com/magang/salesquotation/ui/detail/DetailActivity.java
+++ app/src/main/java/com/magang/salesquotation/ui/detail/DetailActivity.java
@@ -3,8 +3,10 @@ package com.magang.salesquotation.ui.detail;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.magang.salesquotation.R;
+import com.magang.salesquotation.data.entity.Sales;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -15,5 +17,10 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        getData();
+    }
+
+    private void getData(){
+        Sales sales = getIntent().getParcelableExtra(EXTRA_SALES);
     }
 }
