fb576945ee76b718216c88dec39e15364c1ad5e0
tiofani99 <tiofani03@gmail.com>
2020-10-24 21:52:27 +0700

    ganti logo

--- app/src/main/java/com/magang/salesquotation/data/room/SalesRoomDatabase.java
+++ app/src/main/java/com/magang/salesquotation/data/room/SalesRoomDatabase.java
@@ -3,11 +3,9 @@ package com.magang.salesquotation.data.room;
 import android.content.Context;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.magang.salesquotation.data.entity.Sales;
 import com.magang.salesquotation.helper.DateHelper;
@@ -16,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
 
-@Database(entities = {Sales.class},version = 1)
+@Database(entities = {Sales.class}, version = 1)
 public abstract class SalesRoomDatabase extends RoomDatabase {
 
     private static volatile SalesRoomDatabase INSTANCE;
@@ -27,12 +25,6 @@ public abstract class SalesRoomDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             SalesRoomDatabase.class, "sales_database")
-                            .addCallback(new RoomDatabase.Callback() {
-                                @Override
-                                public void onCreate(@NonNull SupportSQLiteDatabase db) {
-                                    add();
-                                }
-                            })
                             .build();
                 }
             }
@@ -46,21 +38,21 @@ public abstract class SalesRoomDatabase extends RoomDatabase {
             @Override
             public void run() {
                 final List<Sales> list = new ArrayList<>();
-                for (int i = 1 ; i <= 100 ; i++){
+                for (int i = 1; i <= 100; i++) {
                     Sales dummySales = new Sales();
-                    dummySales.setItemName("Barang ke "+i);
-                    dummySales.setAmount(""+i);
+                    dummySales.setItemName("Barang ke " + i);
+                    dummySales.setAmount("" + i);
                     dummySales.setDate(DateHelper.getCurrentDate());
-                    dummySales.setDiscount(""+i);
-                    dummySales.setQuantity(""+i);
+                    dummySales.setDiscount("" + i);
+                    dummySales.setQuantity("" + i);
                     dummySales.setUnit("Unit");
                     dummySales.setUnitPrice("100000");
                     dummySales.setTax("false");
-                    dummySales.setDescription("Mantap ke "+i);
+                    dummySales.setDescription("Mantap ke " + i);
                     list.add(dummySales);
                 }
 
-                Log.d("Coba","Membuat Database");
+                Log.d("Coba", "Membuat Database");
 
                 INSTANCE.salesDao().insertAll(list);
             }
